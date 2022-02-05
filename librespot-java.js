4326ad01b5c4b2e61ec7e845bbb13347177fe6d8
Adam <adaand00@gmail.com>
2022-02-05 19:53:07 +0100

    incoming spotify events to UI

--- librespot-java.js
+++ librespot-java.js
@@ -38,7 +38,7 @@ class librespot {
                 break;
 
             case "volumeChanged":
-                this.volume = mess.value;
+                this.volume = Math.round(100 * mess.value);
                 break;
 
             case "playbackPaused":
@@ -68,8 +68,55 @@ class librespot {
     updateDisplay() {
         console.log(this);
 
+        
+
         if(this.status != "stopped"){
-            console.log(this.status + " : " + this.track.name + " at volume " + this.volume);
+            var artists = [];
+            var imgUrl;
+            try {
+                // From spotify API
+                this.track.artists.forEach((art) => {
+                    artists.push(art.name)
+                });
+                imgUrl = this.track.album.images[0].url;
+                
+            } catch (TypeError) {
+                // From librespot API
+                this.track.artist.forEach((art) => {
+                    artists.push(art.name)
+                });
+                var imgID = this.track.album.coverGroup.image[2].fileId;
+                imgUrl = "https://i.scdn.co/image/" + imgID.toLowerCase();
+            }
+
+            this.setTrack(
+                this.track.name,
+                artists,
+                this.track.album.name,
+                this.volume,
+                imgUrl,
+                this.status
+                );
+
+        }else{
+            this.setTrack(
+                "CASE AUDIO",
+                "Open spotify and connect to CASELAB wifi to start",
+                "",
+                0,
+                "./defaultcover.png",
+                this.status
+                );
         }
+    }
+
+    setTrack(title, artists, album, volume, imgUrl, status){
+
+        document.getElementById("title").innerHTML = title;
+        document.getElementById("subtitle").innerHTML = artists.join(", ") + ": " + album;
+        document.getElementById("VolumeSlider").value = volume;
+        document.getElementById("spotify").style.backgroundImage = "url('" + imgUrl + "')";
+        document.getElementById("playPause").children[0].classList = (status == "playing") ? ["fas fa-pause"] : ["fas fa-play"]
+
     }
 }
